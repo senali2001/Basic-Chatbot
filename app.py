0107ece16e84bcec21eae2c0a1cb6e62c332c704
senali2001 <rbnsenali@students.nsbm.ac.lk>
2026-02-18 20:51:02 +0530

    create basic chat bot

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os 
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 
-llm = ChatGroq(api_key = groq_api_key, model="openai/gpt-oss-20b")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are best adviser, give me advice for my education"),
+        ("user","{question}")
+    ]
+)
 
-response = llm.invoke("give me c code for sum of 2 numbers?")
+llm = ChatGroq(api_key = groq_api_key, model="openai/gpt-oss-20b")
+chain = prompt | llm
+response = chain.invoke({"question": "What should I do to improve my learning?"})
 print(response.content)
